4f43172339b11c5a7d0bae064a72f2fce8881d5e
saera <yook.saera@gmail.com>
2025-06-24 22:52:03 +0900

    feat: 인기 테마 페이지 응답 기능 구현

--- src/main/java/roomescape/presentation/controller/HomePageController.java
+++ src/main/java/roomescape/presentation/controller/HomePageController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomePageController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
     @GetMapping("/login")
     public String login() {
         return "login";
